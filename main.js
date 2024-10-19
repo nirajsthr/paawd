ab660a9943d3778d16bcf07980359b4c452854c7
DevNK755 <168348028+nirajsuthar1802@users.noreply.github.com>
2024-10-19 18:49:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('generate').addEventListener('click', function() {
+    const length = document.getElementById('length').value;
+    document.getElementById('password').innerText = generatePassword(length);
+});
+
+function generatePassword(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    return password;
+}
+
